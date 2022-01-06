72e4b90635f6e9f4d7910deb8f533a3783b57f1e
Kevin <k.schneider@outlook.com>
2022-01-06 11:59:23 -0500

    Add basic vscode launch config

---
+++ src/Backend/GameService.Api/Controllers/LobbyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameService.Api.Controllers;
+[Route("api/lobby")]
+[ApiController]
+public class LobbyController : ControllerBase
+{
+    [HttpPost("create")]
+    public IActionResult CreateGame()
+    {
+        return Ok("game-id-goes-here");
+    }
+}
